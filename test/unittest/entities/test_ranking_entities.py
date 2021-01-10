4a72d12b2e25ae92d0224512b1d05ad6695f76af
dudenr33 <3929834+DudeNr33@users.noreply.github.com>
2021-01-10 19:57:40 +0100

    Add unittest for Leaderboard.from_json()

--- test/unittest/entities/test_ranking_entities.py
+++ test/unittest/entities/test_ranking_entities.py
@@ -4,50 +4,53 @@ Created: 22.12.2020
 """
 import pytest
 
-from granturismo_stats.entities.rating import DriverRating, SportsmanshipRating
-from granturismo_stats.entities.profile import User
 from granturismo_stats.entities.ranking import Leaderboard, QualifyingResult
 
 
-@pytest.fixture(name="example_leaderboard")
-def fixture_example_leaderboard():
-    return Leaderboard(
-        entries=[
-            QualifyingResult(
-                user=User(
-                    name="User1",
-                    number=1,
-                    country="de",
-                    driver_rating=DriverRating.A,
-                    sportsmanship_rating=SportsmanshipRating.S,
-                ),
-                score=1234,
-                ranking_id=1,
-            ),
-            QualifyingResult(
-                user=User(
-                    name="User2",
-                    number=2,
-                    country="de",
-                    driver_rating=DriverRating.B,
-                    sportsmanship_rating=SportsmanshipRating.A,
-                ),
-                score=5678,
-                ranking_id=2,
-            ),
-            QualifyingResult(
-                user=User(
-                    name="User3",
-                    number=3,
-                    country="de",
-                    driver_rating=DriverRating.C,
-                    sportsmanship_rating=SportsmanshipRating.B,
-                ),
-                score=4711,
-                ranking_id=3,
-            )
+@pytest.fixture(name="example_json_data")
+def fixture_example_json_data():
+    return {
+        "ranking": [
+            {
+                "user_id":      "User1",
+                "user_no":      1,
+                "user_country": "de",
+                "driver_class": 5,
+                "manner_point": 99,
+                "score":        1234,
+                "ranking_id":   1,
+            },
+            {
+                "user_id":      "User2",
+                "user_no":      2,
+                "user_country": "de",
+                "driver_class": 4,
+                "manner_point": 79,
+                "score":        5678,
+                "ranking_id":   2,
+            },
+            {
+                "user_id":      "User3",
+                "user_no":      3,
+                "user_country": "de",
+                "driver_class": 3,
+                "manner_point": 64,
+                "score":        4714,
+                "ranking_id":   3,
+            }
         ]
-    )
+    }
+
+
+@pytest.fixture(name="example_leaderboard")
+def fixture_example_leaderboard(example_json_data):
+    return Leaderboard.from_json(example_json_data)
+
+
+def test_from_json(example_json_data):
+    leaderboard = Leaderboard.from_json(example_json_data)
+    assert len(leaderboard.entries) == 3
+    assert all(isinstance(entry, QualifyingResult) for entry in leaderboard.entries)
 
 
 def test_to_csv(tmpdir, example_leaderboard):
@@ -57,4 +60,4 @@ def test_to_csv(tmpdir, example_leaderboard):
     assert content == "name;number;country;driver_rating;sportsmanship_rating;score;ranking_id\n" \
                       "User1;1;de;5;S;1234;1\n" \
                       "User2;2;de;4;A;5678;2\n" \
-                      "User3;3;de;3;B;4711;3\n"
+                      "User3;3;de;3;B;4714;3\n"
